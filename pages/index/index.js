6b8c98becd2b9bd5ce49b658110bf874c9ca7c9a
77Vincent <wentianqi77@outlook.com>
2017-08-28 23:44:03 +0800

    add output

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,38 +14,63 @@ Page({
       id: "expense",
       title: "平均月支出"
     }, {
-      id: "incomeRate",
-      title: "年收入增幅"
+      id: "incomeIncreaseRate",
+      title: "年收入增幅(%)",
+      line: true,
     }, {
       id: "interestRateY",
-      title: "理财平均年利率"
+      title: "理财平均年利率(%)",
     }, {
       id: "interestRateM",
-      title: "理财平均月利率"
+      title: "理财平均月利率(%)",
+      line: true,
     }, {
       id: "periodY",
       title: "存多少年"
     }, {
       id: "periodM",
       title: "存多少月"
-    }]
+    }],
+    output: [{
+      id: "deposit",
+      title: "总存款"
+    }, {
+      id: "interest",
+      title: "总利息"
+    }],
+  },
+  convert(e) {
+    if (e.currentTarget.id === "interestRateY") {
+      this.setData({
+        jjj: e.detail.value 
+      });
+    }
   },
   userInput: {},
   getValue(e) {
     this.userInput[e.currentTarget.id] = e.detail.value;
   },
   calculate() {
-    console.log(this.userInput)
+    let de = this.userInput.deposit;
+    let sa = this.userInput.save;
+    let ex = this.userInput.expense;
+    let iir = this.userInput.incomeIncreaseRate;
+    let iry = this.userInput.interestRateY;
+    let irm = this.userInput.interestRateM;
+    let py = this.userInput.periodY;
+    let pm = this.userInput.periodM;
+
+    for (let i = 0; i < pm; i++) {
+      de = de * (1 + irm / 100) + sa;
+    }
+
+    this.setData({
+      result: {
+        deposit: de,
+        interest: de - this.userInput.deposit,
+      }
+    });
   },
   onLoad() {
-    var that = this;
-
-    //调用应用实例的方法获取全局数据
-    app.getUserInfo(function (userInfo) {
-      //更新数据
-      that.setData({
-        userInfo: userInfo
-      });
-    });
   }
 });
