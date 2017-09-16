704df5ab2e48a7aab1658ec02541d8a71c4bb1ad
77Vincent <wentianqi77@outlook.com>
2017-09-16 23:52:28 +0800

    algorithm complete

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,72 +6,95 @@ Page({
   data: {
     input: [{
       id: "deposit",
-      title: "现存款"
+      title: "现存款",
+      placeholder: 0
     }, {
       id: "income",
-      title: "平均月收入"
+      title: "平均月收入",
+      placeholder: 0
     }, {
       id: "expense",
-      title: "平均月支出"
+      title: "平均月支出",
+      placeholder: 0
     }, {
       id: "investmentRate",
-      title: "理财存款占比(%)",
+      title: "理财存款占比 (%)",
+      placeholder: 100 
     }, {
       id: "incomeIncreaseRate",
-      title: "收入年增幅(%)",
+      title: "收入年增幅 (%)",
+      placeholder: 0
     }, {
       id: "interestRate",
-      title: "理财平均年利率(%)",
+      title: "理财平均年利率 (%)",
+      placeholder: 0
     }, {
       id: "period",
-      title: "存多久(年)",
+      title: "存多久 (年)",
+      placeholder: 0
     }],
     output: [{
       id: "deposit",
-      title: "总存款(含利息)"
+      title: "总存款 (含理财收入)"
+    }, {
+      id: "depositWithoutInterest",
+      title: "总存款 (不含理财收入)"
     }, {
       id: "interest",
-      title: "总利息"
+      title: "总理财收入"
+    }, {
+      id: "interestPro",
+      title: "理财收入占总存款比例 (%)"
+    }, {
+      id: "expense",
+      title: "总支出"
     }],
+    done: true
   },
   userInput: {},
   getValue(e) {
     this.userInput[e.currentTarget.id] = e.detail.value === "" ? undefined : Number(e.detail.value);
   },
-  calculate(d=0, inc=0, exp=0, imr=100, iir=0, ir=0, p=0) {
+  calculate(d=0, i=0, e=0, m=100, r2=0, r1=0, n=0) {
 
-    iir = iir / 100;
-    ir = ir / 100;
-    imr = imr / 100;
+    r1 = r1 / 100;
+    r2 = r2 / 100;
+    m = m / 100;
 
-    let iirA = 0;
-    let irA = 0;
-    let siA = 0;
+    const s = (n) => {
+      return 12 * (i * Math.pow(r2 + 1, n - 1) - e);
+    };
 
-    for (let ind = 0; ind < p; ind++) {
-      iirA = iirA + Math.pow((1 + iir), ind);
-      irA = irA + Math.pow(ir, ind + 1);
+    const dr = (x, n) => {
+      x = isNaN(x) ? 0 : x;
 
-      if (ind === 0) {
-        siA = 0;
-      } else {
-        siA = siA + (inc * Math.pow(iir, ind - 1) - exp) * Math.pow(ir, ind);
-      }
+      const ep = Math.pow(10, n);
+      return Math.round(x * ep) / ep;
+    };
+
+    let v = m * r1 + 1;
+    let final = d;
+    let ni = d;
+
+    for (let ind = 1; ind <= n; ind++) {
+      final = final * v + s(n);
+      ni = ni + s(n);
     }
 
-    let base = 12 * (inc * iirA - exp * p) + d;
-    let interest = d * imr * (irA + ir * (p - 1)) + 12 * siA * imr;
-
     return {
-      deposit: Math.round(base + interest),
-      interest: Math.round(interest)
+      deposit: Math.round(final),
+      depositWithoutInterest: Math.round(ni),
+      interest: Math.round(final - ni),
+      interestPro: dr((final - ni) / final, 4) * 100,
+      expense: n * 12 * e
     };
   },
   confirm() {
     let ui = this.userInput;
     
     this.setData({
-      result: this.calculate(ui.deposit, ui.income, ui.expense, ui.investmentRate, ui.incomeIncreaseRate, ui.interestRate, ui.period)
+      result: this.calculate(ui.deposit, ui.income, ui.expense, ui.investmentRate, ui.incomeIncreaseRate, ui.interestRate, ui.period),
+      done: false,
     });
   },
   onLoad() {
