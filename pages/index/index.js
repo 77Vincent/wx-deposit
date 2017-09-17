8878f2a3a350d58f5600baa4f45e94e97e23fae8
77Vincent <wentianqi77@outlook.com>
2017-09-17 23:51:40 +0800

    adding tips

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,6 +7,7 @@ Page({
     input: [{
       id: "deposit",
       title: "现存款",
+      tips: "目前已有存款数目",
       placeholder: 0
     }, {
       id: "income",
@@ -19,7 +20,7 @@ Page({
     }, {
       id: "investmentRate",
       title: "理财存款占比 (%)",
-      placeholder: 100 
+      placeholder: 100
     }, {
       id: "incomeIncreaseRate",
       title: "收入年增幅 (%)",
@@ -30,7 +31,7 @@ Page({
       placeholder: 0
     }, {
       id: "period",
-      title: "存多久 (年)",
+      title: "多少年后",
       placeholder: 0
     }],
     output: [{
@@ -49,18 +50,26 @@ Page({
       id: "expense",
       title: "总支出"
     }],
+    tipsStatus: {
+      deposit: true
+    },
     done: true
   },
   userInput: {},
   getValue(e) {
     this.userInput[e.currentTarget.id] = e.detail.value === "" ? undefined : Number(e.detail.value);
   },
-  calculate(d=0, i=0, e=0, m=100, r2=0, r1=0, n=0) {
+  calculate(d = 0, i = 0, e = 0, m = 100, r2 = 0, r1 = 0, n = 0) {
 
     r1 = r1 / 100;
     r2 = r2 / 100;
     m = m / 100;
 
+    /**
+     * Annual saving
+     * @param {Number} years
+     * @returns saving per year
+     */
     const s = (n) => {
       return 12 * (i * Math.pow(r2 + 1, n - 1) - e);
     };
@@ -85,18 +94,26 @@ Page({
       deposit: Math.round(final),
       depositWithoutInterest: Math.round(ni),
       interest: Math.round(final - ni),
-      interestPro: dr((final - ni) / final, 4) * 100,
+      interestPro: (dr((final - ni) / final, 4) * 100).toFixed(2),
       expense: n * 12 * e
     };
   },
+  showTips(e) {
+    console.log(e)
+  },
   confirm() {
     let ui = this.userInput;
-    
+
     this.setData({
       result: this.calculate(ui.deposit, ui.income, ui.expense, ui.investmentRate, ui.incomeIncreaseRate, ui.interestRate, ui.period),
       done: false,
     });
   },
   onLoad() {
+    this.data.input.map((v, i) => {
+      this.data.tipsStatus[v.id] = true;
+    });
+
+    console.log(this.data.tipsStatus)
   }
 });
